b3167a7dc236b45f2d152ecb974ad847b42a04d7
Hanson Li <hzuoli@gmail.com>
2021-05-23 17:44:26 -0700

    Cleanup and add comments

--- autofiller-take-home/src/content.js
+++ autofiller-take-home/src/content.js
@@ -1,19 +1,22 @@
+/* Heavily referenced from https://github.com/lovincyrus/job-autofiller */
+
+// JSON filenames
 const AUTOFILL_DATA_FILE = chrome.runtime.getURL('data.json');
 const SELECTOR_MAPPING_FILE = chrome.runtime.getURL('selectors.json');
 
+// Autofill data
 let data = {};
+// Mapping of input field name to the corresponding key in the data JSON
 let selectors = {};
 
 const job_url =
   "https://boards.greenhouse.io/simplifyjobsintegrationsandbox/jobs/4344358003";
 
+// Retrieve JSON data
 const dataStream = fetch(AUTOFILL_DATA_FILE)
-  .then(response => response.json())
-  .catch(e => console.log(`failed to read data.json: ${e}`));
-
+  .then(response => response.json());
 const selectorsStream = fetch(SELECTOR_MAPPING_FILE)
-  .then(response => response.json())
-  .catch(e => console.log(`failed to read selectors.json: ${e}`));
+  .then(response => response.json());
 
 Promise.all([
   dataStream,
@@ -24,31 +27,34 @@ Promise.all([
   fillBoard();
 })
 
-function fillBoard() {
+async function fillBoard() {
+  /* For each selector on the greenhouse form mapped in selectors.json, identify
+   * if it is a basic text input or a dropdown, then fill accordingly */
+
   for (let selector in selectors) {
     const inputElement = $(selector);
     if (inputElement.length) {
       const inputData = selectors[selector];
       const inputValue = data[inputData];
+      // TODO: handle school field
       inputElement.each(element => {
         if (inputElement[element].tagName === 'SELECT') {
-          $(selector).click();
           $(`${selector} option:contains("${inputValue}")`).attr("selected", true);
-          var e = jQuery.Event("keydown");
-          e.which = 13;
-          e.keyCode = 13;
-          $(selector).trigger(e);
-          console.log("pressed " + inputValue);
+          // TODO: figure out how to actually set the selected value
+          /* I attempted to trigger a 'change' event as several online sources
+           * have suggested but it doesn't seem to work */
+          // $(selector).val(inputValue);
+          // $(selector).trigger('change');
         } else {
           $(selector).val(inputValue).change();
         }
       });
     }
   }
+
+  // TODO: figure out entire file download/upload process
+  // https://stackoverflow.com/questions/13333378/how-can-javascript-upload-a-blob
+  /* fetch(`https://example.com/upload.php`, {method:"POST", body:blobData})
+              .then(response => console.log(response.text())) */
+
 }
-
-$( document ).ready(function() {
-  $( "#first_name" ).focus();
-});
-
-fillBoard()
